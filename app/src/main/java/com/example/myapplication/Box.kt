2e47adf1ddc19b85cec1a279905dfdb9628f72ec
orangeshop <rhkeh123@naver.com>
2024-06-10 10:23:48 +0900

    box는 공간을 만드는 역할

---
+++ app/src/main/java/com/example/myapplication/Box.kt
@@ -0,0 +1,64 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun box(name: String) {
+//    Box(modifier = Modifier.size(100.dp)) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = Modifier.align(Alignment.BottomEnd)
+//        )
+//    }
+
+//    Box(modifier = Modifier.size(100.dp)) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = Modifier.align(Alignment.BottomEnd)
+//        )
+//        Text(
+//            text = "Hello $name!",
+//            modifier = Modifier.align(Alignment.TopStart)
+//        )
+//    }
+
+//    Box(modifier = Modifier.size(100.dp)) {
+//        Text(
+//            text = "Hello 1!",
+//            modifier = Modifier.align(Alignment.BottomEnd)
+//        )
+//
+//        Text(
+//            text = "Hello 2!",
+//            modifier = Modifier.align(Alignment.CenterEnd)
+//        )
+//
+//        Text(
+//            text = "Hello 3!",
+//            modifier = Modifier.align(Alignment.TopStart)
+//        )
+//
+//    }
+
+//    Box(modifier = Modifier.size(100.dp)) {
+//        Box(modifier = Modifier.size(80.dp).background(Color.Yellow).align(Alignment.CenterStart))
+//        Box(modifier = Modifier.size(80.dp).background(Color.Red).align(Alignment.BottomEnd))
+//    }
+
+    Box() {
+        Box(modifier = Modifier.fillMaxSize().background(Color.Yellow).align(Alignment.CenterStart))
+        Box(modifier = Modifier.size(80.dp).background(Color.Red).align(Alignment.BottomEnd))
+    }
+
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -25,7 +26,9 @@ class MainActivity : ComponentActivity() {
 //                        modifier = Modifier.padding(innerPadding)
 //                    )
 //
-                    surface(name = "Android")
+//                    surface(name = "Android")
+
+                    box("Android")
                 }
             }
         }
@@ -44,6 +47,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        surface(name = "Android")
+//        surface(name = "Android")
+        box(name = "Android")
     }
 }
